95e8ea945e0c8b4a2dbfe7ff4f5efca056925197
Juliette Petit <juliette.petit@u-bordeaux.fr>
2024-06-04 03:47:41 +0200

    multi users current song tracking

--- index.js
+++ index.js
@@ -1,77 +1,96 @@
 require('dotenv').config();
+const uuid = require('uuid').v4;
 const express = require('express');
 const querystring = require('querystring');
 
 var client_id = process.env.CLIENT_ID;
 var client_secret = process.env.CLIENT_SECRET;
 var redirect_uri = process.env.REDIRECT_URI;
-var token = {
-  access_token: null,
-  token_type: null,
-  refresh_token: null,
-  expires_in: null
-};
+var token = {};
 var scope = 'user-read-currently-playing';
 
 var app = express();
 
 app.get('/login', function(req, res) {
   /*var state = generateRandomString(16);*/
+  var state = uuid();
   res.redirect(
       'https://accounts.spotify.com/authorize?' + querystring.stringify({
         response_type: 'code',
         client_id: client_id,
         scope: scope,
         redirect_uri: redirect_uri,
+        state: state,
       }));
 });
 
 app.get('/callback', async function(req, res) {
   var code = req.query.code || null;
   var state = req.query.state || null;
-
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri: redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json',
-      'Authorization': 'Basic ' +
-          (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  if (state === null) {
+    res.redirect('/#' + querystring.stringify({error: 'state_mismatch'}));
+  } else {
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'Authorization': 'Basic ' +
+            (new Buffer.from(client_id + ':' + client_secret)
+                 .toString('base64'))
+      }
+    };
+    let formBody = [];
+    for (var property in authOptions.form) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(authOptions.form[property]);
+      formBody.push(encodedKey + '=' + encodedValue);
     }
-  };
-  let formBody = [];
-  for (var property in authOptions.form) {
-    var encodedKey = encodeURIComponent(property);
-    var encodedValue = encodeURIComponent(authOptions.form[property]);
-    formBody.push(encodedKey + '=' + encodedValue);
-  }
-  formBody = formBody.join('&');
+    formBody = formBody.join('&');
 
-  var options = {headers: authOptions.headers, method: 'POST', body: formBody};
-  var response = await fetch(authOptions.url, options);
-  var json = await response.json();
-  token.access_token = json['access_token'];
-  token.expires_in = json['expires_in'];
-  token.refresh_token = json['refresh_token'];
-  token.token_type = json['token_type'];
+    var options = {
+      headers: authOptions.headers,
+      method: 'POST',
+      body: formBody
+    };
+    var response = await fetch(authOptions.url, options);
+    var json = await response.json();
+    var t = {
+      access_token: json['access_token'],
+      expires_in: json['expires_in'],
+      refresh_token: json['refresh_token'],
+      token_type: json['token_type']
+    };
+    token[state] = t;
+    res.json(state);
+  }
 });
 
 app.get('/current-song', async function(req, res) {
-  if (!token.access_token) {
+  var state = req.query.state || null;
+  if (!state) {
+    res.status(400);
+    res.json({error: 'access denied, please provide a valid state'});
+    return;
+  }
+  if (!token[state]) {
     res.status(403);
     res.json({error: 'access denied, not logged in'});
+    return;
   }
+  console.log(state, token[state]);
   var song = {name: null, album: null, artist: null, date: null, image: null};
   authOptions = {
     url: 'https://api.spotify.com/v1/me/player/currently-playing',
     headers: {
       'Accept': 'application/json',
-      'Authorization': token.token_type + ' ' + token.access_token,
+      'Authorization':
+          token[state].token_type + ' ' + token[state].access_token,
     }
   };
   var options = {headers: authOptions.headers, method: 'GET'};
